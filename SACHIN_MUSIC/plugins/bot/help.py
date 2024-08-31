8a1be6cc942b5a49acbb0aec0b4c25b158e1470f
SACHINMUSIC <sachinxpapa@gmail.com>
2024-08-31 16:13:24 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/plugins/bot/help.py
+++ SACHIN_MUSIC/plugins/bot/help.py
@@ -137,7 +137,7 @@ async def on_back_button(client, CallbackQuery):
         )
 
 @app.on_callback_query(filters.regex('splus'))      
-async def mb_plugin_button(client, CallbackQuery):
+async def sb_plugin_button(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     cb = callback_data.split(None, 1)[1]
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"sbot_cb")]])
