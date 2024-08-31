4afddb5b8f431975ccef1690a1929560d7df98f2
SACHINMUSIC <sachinxpapa@gmail.com>
2024-08-31 15:36:48 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/plugins/bot/help.py
+++ SACHIN_MUSIC/plugins/bot/help.py
@@ -123,7 +123,7 @@ async def mb_plugin_button(client, CallbackQuery):
 
 @app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON))
+    await CallbackQuery.edit_message_text(Helper.HELP_SACHIN, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON))
 
 
 @app.on_callback_query(filters.regex('managebot123'))
