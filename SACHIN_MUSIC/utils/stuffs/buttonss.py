1beec1cf6bdde44d5cba92d7dddfd1bbe318d152
SACHINMUSIC <sachinxpapa@gmail.com>
2024-08-31 15:25:13 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/utils/stuffs/buttonss.py
+++ SACHIN_MUSIC/utils/stuffs/buttonss.py
@@ -4,37 +4,12 @@ from pyrogram import Client, filters, enums
 class BUTTONS(object):
     MBUTTON = [
     [
-        InlineKeyboardButton("+", callback_data="splus HELP_ChatGPT"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Group"),
-        InlineKeyboardButton("+", callback_data="mplus HELP_Sticker")
-    ],
-    [
-        InlineKeyboardButton("+", callback_data="splus HELP_TagAll"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Info"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Extra")
-    ],
-    [
-        InlineKeyboardButton("+", callback_data="splus HELP_Image"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Action"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Search")
-    ],    
-    [
-        InlineKeyboardButton("+", callback_data="splus HELP_Font"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Game"),
-        InlineKeyboardButton("+", callback_data="splus HELP_TG")
-    ],
-    [
-        InlineKeyboardButton("+", callback_data="splus HELP_Imposter"),
-        InlineKeyboardButton("+", callback_data="splus HELP_TD"),
-        InlineKeyboardButton("+", callback_data="splus HELP_HT")
-    ], 
-    [
-        InlineKeyboardButton("+", callback_data="splus HELP_TTS"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Fun"),
-        InlineKeyboardButton("+", callback_data="splus HELP_Q")
+        InlineKeyboardButton("+", callback_data="mplus HELP_SACHIN"),
+        InlineKeyboardButton("+", callback_data="mplus HELP_MUSIC"),
+        InlineKeyboardButton("+", callback_data="mplus HELP_SANATANI")
     ],          
     [
         InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"settings_back_helper"), 
-        InlineKeyboardButton("ɴᴇxᴛ", callback_data=f"managebot1234 settings_back_helper"),
+        InlineKeyboardButton("ɴᴇxᴛ", callback_data=f"splus"),
     ]
     ]
