2087965974a00b37510ef7e26db3d0aa8728d870
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-01 23:19:04 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/utils/inline/start.py
+++ SACHIN_MUSIC/utils/inline/start.py
@@ -22,7 +22,10 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="mplus")
+        
+        ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
