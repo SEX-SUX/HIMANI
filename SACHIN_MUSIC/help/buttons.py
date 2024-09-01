087d7521c9cc6ab92aa98c93aa9818a37da126e3
SACHINMUSIC <sachinxpapa@gmail.com>
2024-09-02 03:33:00 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/help/buttons.py
+++ SACHIN_MUSIC/help/buttons.py
@@ -10,22 +10,22 @@ class BUTTONS(object):
     ],
     [
         InlineKeyboardButton("ɪɴꜰᴏ", callback_data="mplus HELP_H4"),
-        InlineKeyboardButton("ᴛʀᴘʜ", callback_data="mplus HELP_H5")
-        InlineKeyboardButton("ᴇxᴛʀᴀ", callback_data="mplus HELP_H6")
+        InlineKeyboardButton("ᴛʀᴘʜ", callback_data="mplus HELP_H5"),
+        InlineKeyboardButton("ᴇxᴛʀᴀ", callback_data="mplus HELP_H6"),
     ],
     [
         InlineKeyboardButton("ᴛᴀɢᴀʟʟ", callback_data="mplus HELP_H7"),
-        InlineKeyboardButton("ʜsᴛᴀɢ", callback_data="mplus HELP_H8")
+        InlineKeyboardButton("ʜsᴛᴀɢ", callback_data="mplus HELP_H8"),
         InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_H9"),
     ],
     [
         InlineKeyboardButton("ɪᴍᴀɢᴇ", callback_data="mplus HELP_H10"),
-        InlineKeyboardButton("sᴛᴄᴋʀs", callback_data="mplus HELP_H11")
+        InlineKeyboardButton("sᴛᴄᴋʀs", callback_data="mplus HELP_H11"),
         InlineKeyboardButton("ɪᴍᴘsᴛʀ", callback_data="mplus HELP_H12"),
     ],    
     [
         InlineKeyboardButton("ғᴜɴs", callback_data="mplus HELP_H13"),
-        InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_H14")
+        InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_H14"),
     ],          
     [
         InlineKeyboardButton("⌯ ʙᴀᴄᴋ ⌯", callback_data=f"settingsback_helper",),
