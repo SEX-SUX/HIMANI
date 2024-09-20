850174b1bba6027aba295a04b987b47f4c369f70
SEX-SUX <sachinxpapa@gmail.com>
2024-09-20 05:44:58 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/help/buttons.py
+++ SACHIN_MUSIC/help/buttons.py
@@ -2,32 +2,73 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
+    BBUTTON = [
+        [
+            InlineKeyboardButton("ᴧɪ | ᴄʜᴧᴛɢᴘᴛ", callback_data="TOOL_BACK HELP_01"),
+        ],
+        [
+            InlineKeyboardButton("sєᴧʀᴄʜ", callback_data="TOOL_BACK HELP_02"),
+            InlineKeyboardButton("ᴛᴛs", callback_data="TOOL_BACK HELP_03"),
+            InlineKeyboardButton("ɪηғσ", callback_data="TOOL_BACK HELP_04"),
+        ],
+        [
+            InlineKeyboardButton("ғσηᴛ", callback_data="TOOL_BACK HELP_05"),
+            InlineKeyboardButton("ϻᴧᴛʜ", callback_data="TOOL_BACK HELP_06"),
+            InlineKeyboardButton("ᴛᴧɢᴧʟʟ", callback_data="TOOL_BACK HELP_07"),
+        ],
+        [
+            InlineKeyboardButton("ɪϻᴧɢє", callback_data="TOOL_BACK HELP_08"),
+            InlineKeyboardButton("ʜᴧsᴛᴧɢ", callback_data="TOOL_BACK HELP_09"),
+            InlineKeyboardButton("sᴛɪᴄᴋєʀs", callback_data="TOOL_BACK HELP_10"),
+        ],
+        [
+            InlineKeyboardButton("ғυη", callback_data="TOOL_BACK HELP_11"),
+            InlineKeyboardButton("ǫυσᴛʟʏ", callback_data="TOOL_BACK HELP_12"),
+            InlineKeyboardButton("ᴛ-ᴅ", callback_data="TOOL_BACK HELP_13"),
+        ],
+        [   
+            InlineKeyboardButton("• ʙᴧᴄᴋ •", callback_data=f"MAIN_CP"),]
+        ]
+    
     MBUTTON = [
-    [
-        InlineKeyboardButton("ᴀᴄᴛɪᴏɴ", callback_data="mplus HELP_H1"),
-        InlineKeyboardButton("ɢʀᴘs", callback_data="mplus HELP_H2"),
-        InlineKeyboardButton("ᴀɪ", callback_data="mplus HELP_H3"),
-    ],
-    [
-        InlineKeyboardButton("ɪɴꜰᴏ", callback_data="mplus HELP_H4"),
-        InlineKeyboardButton("ᴛʀᴘʜ", callback_data="mplus HELP_H5"),
-        InlineKeyboardButton("ᴇxᴛʀᴀ", callback_data="mplus HELP_H6"),
-    ],
-    [
-        InlineKeyboardButton("ᴛᴀɢᴀʟʟ", callback_data="mplus HELP_H7"),
-        InlineKeyboardButton("ʜsᴛᴀɢ", callback_data="mplus HELP_H8"),
-        InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_H9"),
-    ],
-    [
-        InlineKeyboardButton("ɪᴍᴀɢᴇ", callback_data="mplus HELP_H10"),
-        InlineKeyboardButton("sᴛᴄᴋʀs", callback_data="mplus HELP_H11"),
-        InlineKeyboardButton("ɪᴍᴘsᴛʀ", callback_data="mplus HELP_H12"),
-    ],    
-    [
-        InlineKeyboardButton("ғᴜɴs", callback_data="mplus HELP_H13"),
-        InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_H14"),
-    ],          
-    [
-        InlineKeyboardButton("⌯ ʙᴀᴄᴋ ⌯", callback_data=f"settingsback_helper",),
-    ]
-    ]
+                [
+            InlineKeyboardButton("єxᴛʀᴧ", callback_data="MANAGEMENT_BACK HELP_25"),
+        ],
+        [
+            InlineKeyboardButton("ʙᴧη", callback_data="MANAGEMENT_BACK HELP_14"),
+            InlineKeyboardButton("ᴋɪᴄᴋ", callback_data="MANAGEMENT_BACK HELP_15"),
+            InlineKeyboardButton("ϻυᴛє", callback_data="MANAGEMENT_BACK HELP_16"),
+        ],
+        [
+            InlineKeyboardButton("ᴘɪη", callback_data="MANAGEMENT_BACK HELP_17"),
+            InlineKeyboardButton("sᴛᴧғғ", callback_data="MANAGEMENT_BACK HELP_18"),
+            InlineKeyboardButton("sєᴛ-υᴘ", callback_data="MANAGEMENT_BACK HELP_19"),
+        ],
+        [
+            InlineKeyboardButton("ᴢσϻʙɪє", callback_data="MANAGEMENT_BACK HELP_20"),
+            InlineKeyboardButton("ɢᴧϻє", callback_data="MANAGEMENT_BACK HELP_21"),
+            InlineKeyboardButton("ɪϻᴘσsᴛєʀ", callback_data="MANAGEMENT_BACK HELP_22"),
+        ],
+        [
+            InlineKeyboardButton("sɢ", callback_data="MANAGEMENT_BACK HELP_23"),
+            InlineKeyboardButton("ᴛʀ", callback_data="MANAGEMENT_BACK HELP_24"),
+            InlineKeyboardButton("ɢʀᴧᴘʜ", callback_data="MANAGEMENT_BACK HELP_26"),
+        ],
+        [
+            InlineKeyboardButton("• ʙᴧᴄᴋ •", callback_data=f"MAIN_CP"), 
+        ]
+        ]
+    SBUTTON = [
+        [
+            InlineKeyboardButton("ϻυsɪᴄ", callback_data="settings_back_helper"),
+            InlineKeyboardButton("ϻᴧηᴧɢєϻєηᴛ", callback_data="MANAGEMENT_CP"),
+        ],
+        [
+            InlineKeyboardButton("ᴛσσʟs", callback_data="TOOL_CP"),
+            InlineKeyboardButton("ᴘʀσϻσ", callback_data="MAIN_BACK HELP_27"),
+        ],
+        [
+            InlineKeyboardButton("ʙᴧᴄᴋ ᴛσ ʜσϻє", callback_data="settingsback_helper"),
+            
+        ]
+        ]
