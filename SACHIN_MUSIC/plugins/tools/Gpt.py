7afca2ff4f6c09e96f02a2668e5f4884d4e16768
SEX-SUX <sachinxpapa@gmail.com>
2024-09-20 18:44:45 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/plugins/tools/Gpt.py
+++ SACHIN_MUSIC/plugins/tools/Gpt.py
@@ -1,37 +1,26 @@
-import asyncio
-import aiohttp
-from pyrogram import Client, filters
+import os
+import random
+import time
 from SACHIN_MUSIC import app
-from pymongo import MongoClient
-from config import MONGO_DB_URI
+import requests
+from pyrogram.types import  Message
+from pyrogram.types import InputMediaPhoto
+from SACHINxAPI import api
+from pyrogram.enums import ChatAction, ParseMode
+from pyrogram import filters
 
-DATABASE = MongoClient(MONGO_DB_URI)
-db = DATABASE["MAIN"]["USERS"]
-collection = db["members"]
 
-def add_user_database(user_id: int):
-    check_user = collection.find_one({"user_id": user_id})
-    if not check_user:
-        return collection.insert_one({"user_id": user_id})
-
-async def chat_with_api(question):
-    url = f"https://chatgpt.apiitzasuraa.workers.dev/?question={question}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
-                if data["code"] == 2:
-                    return data["content"]
-                else:
-                    return "ᴇʀʀᴏʀ: ᴜɴᴀʙʟᴇ ᴛᴏ ɢᴇᴛ ʀᴇꜱᴘᴏɴꜱᴇ ꜰʀᴏᴍ ᴛʜᴇ ᴀᴘɪ"
-            else:
-                return "ᴇʀʀᴏʀ: ᴜɴᴀʙʟᴇ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ᴛʜᴇ ᴀᴘɪ"
-                
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["/", ".", "!", "?"]))
-async def gptAi(client, message):
-    split_text = message.text.split(None, 1)
-    if len(split_text) < 2:
-        await message.reply_text("❖ ᴜꜱᴀɢᴇ : /ai [ǫᴜᴇʀʏ]")
-    else:
-        response = await chat_with_api("gpt", split_text[1])
-        await message.reply_text(response)
+@app.on_message(filters.command(["chatgpt", "ai", "ask", "gpt", "solve"], prefixes=["+", ".", "/", "-", "", "$", "#", "&"],))
+async def chat_gpt(bot, message):
+    
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**❖ ᴜꜱᴀɢᴇ : /ai [ǫᴜᴇʀʏ]**")
+        else:
+            a = message.text.split(' ', 1)[1]
+            r=api.gemini(a)["results"]
+            await message.reply_text(f" {r} \n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ @All_SANATANI_BOT", parse_mode=ParseMode.MARKDOWN)     
+    except Exception as e:
+        await message.reply_text(f"**❖ ᴇʀʀᴏʀ: {e} ")
