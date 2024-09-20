f92d83bc09e1ec1045b6d4cff721853e3a5cc681
SEX-SUX <sachinxpapa@gmail.com>
2024-09-20 05:50:46 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHIN_MUSIC/utils/inline/start.py
+++ SACHIN_MUSIC/utils/inline/start.py
@@ -22,15 +22,14 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_3"],url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="mbot_cb")
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="MAIN_CP"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
